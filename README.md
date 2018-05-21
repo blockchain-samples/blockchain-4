ff19cd4580e801c87e6f18dd61ff7e394cd5ce16
Dowon Cha <dowoncha.cs@gmail.com>
2018-05-21 19:40:36 -0400

    added mining and tamper proof

---
+++ README.md
@@ -0,0 +1,10 @@
+# Blockchain
+
+Simple blockchain implementation.
+* Blocks store strings of data
+* Has a digital signature to chain blocks together
+* Requires proof of work mining to validate new blocks
+* Check if data is valid and unchanged
+
+Thanks to 
+https://medium.com/programmers-blockchain/create-simple-blockchain-java-tutorial-from-scratch-6eeed3cb03fa
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,14 @@ use crypto::sha2::Sha256;
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::fmt;
 
+static DIFFICULTY: usize = 2;
+
 struct Block {
     hash: String,
     pub previous_hash: String,
     pub data: String,
-    pub timestamp: u64
+    pub timestamp: u64,
+    nonce: i32
 }
 
 impl Block {
@@ -21,27 +24,47 @@ impl Block {
         let timestamp = since_the_epoch.as_secs() * 1000 +
             since_the_epoch.subsec_nanos() as u64 / 1_000_000;
 
-        let hash = Self::calculate_hash(previous_hash, timestamp, data);
+        let nonce = 0;
+
+        let hash = Self::calculate_hash(previous_hash, timestamp, nonce, data);
 
         Self {
             data: data.to_string(),
             hash,
             previous_hash: previous_hash.to_string(),
-            timestamp 
+            timestamp,
+            nonce
         }
     }
 
-    pub fn calculate_hash(previous_hash: &str, timestamp: u64, data: &str) -> String {
+    pub fn calculate_hash(previous_hash: &str, timestamp: u64, nonce: i32, data: &str) -> String {
         let mut sha = Sha256::new();
-        let input = format!("{}{}{}", previous_hash, timestamp, data);
+        let input = format!("{}{}{}{}", previous_hash, timestamp, nonce, data);
         sha.input_str(&input);
         let hash = sha.result_str();
 
         hash
     }
 
-    pub fn hash(&self) -> &str {
-        &self.hash
+    pub fn hash(&self) -> String {
+        Self::calculate_hash(
+            &self.previous_hash, 
+            self.timestamp, 
+            self.nonce, 
+            &self.data
+        )
+        // &self.hash
+    }
+
+    pub fn mine_block(&mut self, difficulty: usize) {
+        let target = "0".repeat(difficulty);
+
+        while &self.hash()[..difficulty] != target {
+            self.nonce += 1;
+            self.hash = self.hash();
+        }
+
+        println!("Block mined: {}", self.hash);
     }
 }
 
@@ -56,6 +79,14 @@ impl Blockchain {
         }
     }
 
+    pub fn blocks(&self) -> &[Block] {
+        &self.blocks
+    }
+
+    pub fn blocks_mut(&mut self) -> &mut [Block] {
+        &mut self.blocks
+    }
+
     pub fn push(&mut self, block: Block) {
         self.blocks.push(block);
     }
@@ -65,17 +96,27 @@ impl Blockchain {
     }
 
     pub fn is_chain_valid(&self) -> bool {
+        let hash_target = "0".repeat(DIFFICULTY);
+
         for i in 1..self.blocks.len() {
             let current_block = self.blocks.get(i).expect("No blocks found");
             let previous_block = self.blocks.get(i - 1).expect("Need at least 2 blocks");
 
-            if current_block.hash() != Block::calculate_hash(&current_block.previous_hash, current_block.timestamp, &current_block.data) {
+            // Compare registered hash and calculated hash
+            if current_block.hash() != Block::calculate_hash(&current_block.previous_hash, current_block.timestamp, current_block.nonce, &current_block.data) {
                 println!("Current hashes not equal");
                 return false;
             }
 
+            // Compare previous hash and registered previous hash
             if previous_block.hash() != current_block.previous_hash {
                 println!("Previous hashes not equal");
+                return false;
+            }
+
+            // check if hash is solved
+            if &current_block.hash()[..DIFFICULTY] != hash_target {
+                println!("Block has not been mined");
                 return false;
             }
         }
@@ -97,23 +138,35 @@ impl fmt::Display for Blockchain {
 fn main() {
     let mut blockchain = Blockchain::new();
 
-    blockchain.push(Block::new("I AM THE GENESIS BLOCK, THE ONE AND ONLY.", "0"));
+    {
+        blockchain.push(Block::new("I AM THE GENESIS BLOCK, THE ONE AND ONLY.", "0"));
+    }
+
+    println!("Trying to mine block 1");
+    blockchain.blocks_mut().get_mut(0).unwrap().mine_block(DIFFICULTY);
 
     {
-        let second_block = Block::new("Second block mofos", 
-        blockchain.last().unwrap().hash());
+        let second_block = Block::new(
+            "Second block mofos", 
+            &blockchain.last().unwrap().hash());
 
         blockchain.push(second_block);
     }
 
+    println!("Trying to mine block 2");
+    blockchain.blocks_mut().get_mut(1).unwrap().mine_block(DIFFICULTY);
+
     {
-        let block = Block::new("Third block up in the house", 
-            blockchain.last().unwrap().hash());
+        let block = Block::new(
+            "Third block up in the house", 
+            &blockchain.last().unwrap().hash());
 
         blockchain.push(block);
     }
 
-    println!("{}", blockchain);
+    println!("Trying to mine block 3");
+    blockchain.blocks_mut().get_mut(2).unwrap().mine_block(DIFFICULTY);
+
     if blockchain.is_chain_valid() {
         println!("Chain is valid");
     } else {
