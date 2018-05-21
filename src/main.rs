2003150bde1bc64b813a4a7f53fadd8d54b2d514
Dowon Cha <dowoncha.cs@gmail.com>
2018-05-21 19:18:13 -0400

    added block chain validation

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use crypto::digest::Digest;
 use crypto::sha2::Sha256;
 
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::fmt;
 
 struct Block {
     hash: String,
     pub previous_hash: String,
-    data: String,
-    timestamp: u64
+    pub data: String,
+    pub timestamp: u64
 }
 
 impl Block {
@@ -44,19 +45,78 @@ impl Block {
     }
 }
 
-pub type Blockchain = Vec<Block>;
+struct Blockchain {
+    blocks: Vec<Block>
+}
 
 impl Blockchain {
+    pub fn new() -> Self {
+        Self {
+            blocks: Vec::new()
+        }
+    }
 
+    pub fn push(&mut self, block: Block) {
+        self.blocks.push(block);
+    }
+
+    pub fn last(&self) -> Option<&Block> {
+        self.blocks.last()
+    }
+
+    pub fn is_chain_valid(&self) -> bool {
+        for i in 1..self.blocks.len() {
+            let current_block = self.blocks.get(i).expect("No blocks found");
+            let previous_block = self.blocks.get(i - 1).expect("Need at least 2 blocks");
+
+            if current_block.hash() != Block::calculate_hash(&current_block.previous_hash, current_block.timestamp, &current_block.data) {
+                println!("Current hashes not equal");
+                return false;
+            }
+
+            if previous_block.hash() != current_block.previous_hash {
+                println!("Previous hashes not equal");
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+impl fmt::Display for Blockchain {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for (index, block) in self.blocks.iter().enumerate() {
+            writeln!(f, "Block {} Hash: {}", index, block.hash());
+        }
+
+        Ok(())
+    }
 }
 
 fn main() {
-    let mut blockchain = Vec::new();
+    let mut blockchain = Blockchain::new();
 
     blockchain.push(Block::new("I AM THE GENESIS BLOCK, THE ONE AND ONLY.", "0"));
-    blockchain.push(Block::new("Second block mofos", 
-        blockchain.last().unwrap().hash());
-    blockchain.push(Block::new("Third block up in the house", 
+
+    {
+        let second_block = Block::new("Second block mofos", 
         blockchain.last().unwrap().hash());
 
+        blockchain.push(second_block);
+    }
+
+    {
+        let block = Block::new("Third block up in the house", 
+            blockchain.last().unwrap().hash());
+
+        blockchain.push(block);
+    }
+
+    println!("{}", blockchain);
+    if blockchain.is_chain_valid() {
+        println!("Chain is valid");
+    } else {
+        println!("Uh oh! Chain is invalid");
+    }
 }
